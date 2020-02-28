1da8f294db133f68b466c9736ded674b3047d11e
jjtdeveloper <jjtdeveloper@gmail.com>
2020-02-28 21:05:15 +0000

    Created input values

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 # Author: Jordan Torres
 # Date: 2/28/20
+
+# input will be a list of all members names, section, and height
+input = [
+    ["Alex M", "t1", (6, 0)], 
+    ["Lina", "s1", (5, 9)],
+    ["Aiden", "b1", (5, 11)],
+    ["Katie", "a2", (6, 1)],
+    ["Asher", "b2", (6, 7)],
+    ["jordan", "t2", (5, 10)],
+    ["Alex S", "t2", (5, 9)],
+    ["Kendal", "s1", (5, 4)],
+    ["Rondea", "b1", (5, 10)],
+    ["Ethan", "t1", (5, 8)],
+    ["Sophie", "s1", (5, 6)],
+    ["Emma", "a2", (5, 6)],
+    ["Zoe", "s2", (5, 6)],
+    ["Sawyer", "s2", (5, 5)],
+    ["Neziah", "a2", (5, 7)],
+    ["Breyon", "a1", (5, 5)],
+    ["James", "a1", (5, 8)],
+    ["Celia", "a1", (5, 6)],
+    ["Tyren", "b2", (5, 5)],
+    ["J'Viohn", "b2", (5, 10)]
+    ]
